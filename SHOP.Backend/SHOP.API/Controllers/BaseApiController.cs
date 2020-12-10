494bbbe519531341a979ab6e730604a9c6236fce
sonag <sonag@pm.me>
2020-12-10 19:45:39 +0700

    refactor projects

---
+++ SHOP.Backend/SHOP.API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SHOP.API.Controllers
+{
+    public class BaseApiController
+    {
+    }
+}
